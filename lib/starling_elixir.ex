a733345bcc86be73c6b66ef827f5fb32c8a566d1
Alex Heeton <alex@heeton.me>
2018-01-05 18:33:10 +0000

    Initialising: Baby's first elixir library.
    I'm pretty new to Elixir and this is my first lib. Apologies for
    anything bizarre!

---
+++ lib/starling_elixir.ex
@@ -0,0 +1,18 @@
+defmodule StarlingElixir do
+  @moduledoc """
+  Documentation for StarlingElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StarlingElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/starling_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule StarlingElixirTest do
+  use ExUnit.Case
+  doctest StarlingElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
