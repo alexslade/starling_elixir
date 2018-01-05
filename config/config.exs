48f81fdba9a7a19ce84cc47603a61863311855c4
Alex Heeton <alex@heeton.me>
2018-01-05 23:05:45 +0000

    Add the starling API endpoint

--- config/config.exs
+++ config/config.exs
@@ -8,6 +8,8 @@ use Mix.Config
 # if you want to provide default values for your application for
 # 3rd-party users, it should be done in your "mix.exs" file.
 
+config :starling, endpoint: "https://api.starlingbank.com"
+
 # You can configure for your application as:
 #
 #     config :starling, key: :value
