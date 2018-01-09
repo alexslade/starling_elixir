b1128a4192a2c4191fae386fa89f2c8ae5f903cb
Alex Heeton <alex@heeton.me>
2018-01-09 23:01:52 +0000

    Remove private access tokens
    I’ve cycled the private access token and application secret as well.

--- test/starling/transaction_test.exs
+++ test/starling/transaction_test.exs
@@ -7,9 +7,9 @@ defmodule Starling.TransactionTest do
   test "list/1 returns transactions" do
     {:ok, client} =
       Starling.Client.personal_access_client(
-        "qrynyLp5eFfYadq5rVSTCw16U8k2pqvJe1CN2X3GJ9mZunnhxtrSdSkWtoMFDKi2", # TODO: don't commit these
-        "lvZ9lt6xDSyWSjxYz4tE",
-        "FmfJttxxpR1StXb2eA7nNobKsNBLPR1Pat2wzwS1"
+        Application.get_env(:starling, :personal_access_token),
+        Application.get_env(:starling, :client_id),
+        Application.get_env(:starling, :client_secret)
       )
 
     {:ok, transactions} = Transaction.list(client)
